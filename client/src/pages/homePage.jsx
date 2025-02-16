bab538bad2ab652a457be23a356cb03b1a801149
rick <taariq.connect@gmail.com>
2025-02-16 23:10:30 +0100

    updated email address

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -35,7 +35,7 @@ function Home ({user, yoffset, setYoffset}) {
     useEffect(() => {
       axios.get(url + '/photos/all')
       .then((result) => {
-          console.log(results)
+          console.log(result)
           let results = result.data.reverse()
 
           setPhotos(results); 
