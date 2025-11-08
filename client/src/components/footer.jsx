a03e41a233790a6d5193205245bf3c92e1088343
rick <taariq.connect@gmail.com>
2025-11-08 14:34:39 +0100

    footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer () {
     <footer style = {{width: '100%', backgroundColor: '#f1f1f1', textAlign: 'center', padding: 15, minWidth: 600}}>
         <p>
         <span style = {{verticalAlign: 'middle'}}>&copy; </span>
-        <a href="https://x.com/si_lentCoder" style = {{color: "black",textDecoration: "none"}}> SI LENTCODER</a>
+        <a href="https://x.com/si_lentCoder" style = {{color: "black",textDecoration: "none"}}> taariq.connect@gmail.com</a>
         </p>
     </footer>
     )
